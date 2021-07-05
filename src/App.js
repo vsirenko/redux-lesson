6e1faccc859755b18393d30b9cbf46524695a4c6
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2021-07-05 21:40:54 +0300

    first try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react'
+import{useDispatch, useSelector} from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+ 
+   const addCash = (cash) => {
+    dispatch({type: "ADD_CASH", payload: cash})
+    console.log('LOL');
+   }
+   const getCash = (cash) => {
+    dispatch({type: "GET_CASH", payload: cash})
+    console.log('CLICK');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'> 
+      <div className='cash'>
+      {cash}
+      </div>
+     <div className="btn-all">
+        <button className='btn' onClick={() => addCash(Number(prompt()))}>Добавить</button>
+        <button className='btn' onClick={() => getCash(Number(prompt()))}>Отянть</button>
+     </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
