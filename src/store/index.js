3f07411500fea4562efe98341cb12371d2b4b889
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2021-07-05 22:19:43 +0300

    Added devTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import {createStore, combineReducers} from 'redux'
 import {CashReducer} from './CashReducer'
 import {CustomerReducer} from './CustomerReducer'
+import {composeWithDevTools } from 'redux-devtools-extension'
 
 const rootReducer = combineReducers({
     cash: CashReducer,
     customer: CustomerReducer
 })
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools());
